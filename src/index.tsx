cf5215266349539c7ed7be7686194ab2513a5014
Hugh Middleton <hughfmiddleton@gmail.com>
2020-10-30 20:32:32 -0700

    Add examples

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import CardHeader, { CardHeaderProps } from '@material-ui/core/CardHeader'
+
+export const StringExample: React.FC = () => {
+  return (
+    <CardHeader title="String Title" />
+  );
+}
+
+export const ElementExample: React.FC = () => {
+  return (
+    <CardHeader title={<div>Element Title</div>} />
+  );
+}
+
+export const StringExampleProps: CardHeaderProps = {
+  title: "String Title",
+};
+
+export const ElementExampleProps: CardHeaderProps = {
+  title: <div>Element Title</div>,
+}
